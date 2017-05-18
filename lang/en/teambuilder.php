85243817f8ecc79435fc879732370986dc9ebc48
Adam Olley <adam.olley@blackboard.com>
2017-05-19 09:24:54 +0930

    add help text for the module

--- lang/en/teambuilder.php
+++ lang/en/teambuilder.php
@@ -18,3 +18,9 @@ $string['teambuilder:addinstance'] = 'Add a new teambuilder module';
 $string['teambuilder:build'] = 'Build teams from survey response';
 $string['teambuilder:create'] = 'Create survey';
 $string['teambuilder:respond'] = 'Respond to Questionnaire';
+$string['modulename_help'] = 'Teambuilder is a tool to assign students to Moodle groups based on their answers to a series of questions that you specify.
+The idea is to:
+
+* define the different roles to be performed within the group activity
+* formulate multiple-choice questions so as to identify the students best suited to filling those roles, then
+* the tool will distribute those students evenly among a specified number of teams.';
